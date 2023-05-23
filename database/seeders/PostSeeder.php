115023964545e9d61e997bdabbe8d9b6c794246a
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-05-23 09:57:17 +0200

    Grazie a San Simone seeder debuggato :)

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -17,7 +17,7 @@ class PostSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i = 0; $i > 10; $i++) {
+        for ($i = 0; $i < 10; $i++) {
             $newPost = new Post();
             $newPost->title = $faker->sentence(12);
             $newPost->article = $faker->text(400);
